82df8ec27cd605484fad1920a6e70568c21f8012
Jason Yu <jasonyu0100@gmail.com>
2018-10-30 11:53:19 +1100

    Working API data retriever w/ conversion to dictionary

--- Djikstra/api.py
+++ Djikstra/api.py
@@ -1,5 +1,5 @@
 import urllib.request
 req = urllib.request.Request("http://overpass-api.de/api/interpreter?data=way(-33.910,151.106,-33.900,151.116);out;")
-res = urllib.request.urlopen(req).read()
+res = str(urllib.request.urlopen(req).read(),"ascii")
 with open('ways.txt',"w") as f:
     print(res,file=f)
