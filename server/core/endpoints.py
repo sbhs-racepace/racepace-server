708448ac04c99ba9213ac510c4d8ec67bf2243f4
verixx <abdurraqeeb53@gmail.com>
2018-11-03 23:16:49 +1100

    fixed a typo in endpoints

--- server/core/endpoints.py
+++ server/core/endpoints.py
@@ -1,4 +1,4 @@
 class Overpass:
-    BASE = 'http://overpass-api.de/api/interpreter?data=[out:json]'
+    BASE = 'http://overpass-api.de/api/interpreter?data=[out:json];'
     NODE = BASE + 'node({});out;'
     WAY = BASE + 'way({});out;'
