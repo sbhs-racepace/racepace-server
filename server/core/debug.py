c0ac27a38846da270e4c3de741d33f0dfc17fcac
verixx <abdurraqeeb53@gmail.com>
2018-11-04 17:20:42 +1100

    Add pipfile and pipfile.lock

--- server/core/debug.py
+++ server/core/debug.py
@@ -1,25 +1,28 @@
 import time
 import inspect
+from functools import wraps
 
-def timed():
-    def decorator(f):
-        coro = inspect.iscoroutinefunction(f)
 
-        def wrapper(*args, **kwargs):
-            time1 = time.time()
-            ret = f(*args, **kwargs)
-            time2 = time.time()
-            time_taken = (time2-time1)*1000.0
-            print(f'{f.__name__:s} function took {time_taken:.3f} ms')
-            return ret
+def timed(f):
+    coro = inspect.iscoroutinefunction(f)
 
-        async def async_wrapper(*args, **kwargs):
-            time1 = time.time()
-            ret = await f(*args, **kwargs)
-            time2 = time.time()
-            time_taken = (time2-time1)*1000.0
-            print(f'{f.__name__:s} function took {time_taken:.3f} ms')
-            return ret
-        
-        return async_wrapper if coro else wrapper
-    return decorator
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        time1 = time.time()
+        ret = f(*args, **kwargs)
+        time2 = time.time()
+        time_taken = (time2-time1)*1000.0
+        print(f'{f.__name__:s} function took {time_taken:.3f} ms')
+        return ret
+
+    @wraps(f)
+    async def async_wrapper(*args, **kwargs):
+        time1 = time.time()
+        ret = await f(*args, **kwargs)
+        time2 = time.time()
+        time_taken = (time2-time1)*1000.0
+        print(f'{f.__name__:s} function took {time_taken:.3f} ms')
+        return ret
+    
+    return async_wrapper if coro else wrapper
+
