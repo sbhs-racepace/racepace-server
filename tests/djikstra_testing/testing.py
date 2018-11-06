a91daac49724960754fdd95f6beb133f28126e9d
Jason Yu <jasonyu0100@gmail.com>
2018-11-06 21:45:24 +1100

    Basic Djikstra Neighbour Setup

--- tests/djikstra_testing/testing.py
+++ tests/djikstra_testing/testing.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import math
+from math import inf
 LOCATION = "-33.901,151.106,-33.900,151.107"
 EARTH_RADIUS = 6373000
 
@@ -32,20 +33,66 @@ def node_dist(node1,node2):
 
 ways_file = open('ways.txt','r')
 ways_json = json.loads(ways_file.read())
-# print(ways)
+ways = ways_json['elements']
 
 nodes_file = open('nodes.txt','r')
 nodes_json = json.loads(nodes_file.read())
 nodes = nodes_json['elements']
-new_nodes = {}
+
+neighbours = dict()
+roads = dict()
 for node in nodes:
-    _,id,lat,lon = node.values()
-    print(id,lat,lon)
-    new_nodes[id] = {'lat':lat,'lon':lon}
+    neighbours[node['id']] = set()
+
+for way in ways:
+    way_nodes = way['nodes']
+    for current in range(len(way_nodes)):
+        if way_nodes[current] not in neighbours:
+            continue
+            # neighbours[way_nodes[current]] = set()
+        previous = current - 1 if current > 0 else None
+        next = current + 1 if current + 1 < len(way_nodes) else None
+        current_neighbours = {way_nodes[index] for index in [previous,next] if index != None}
+        neighbours[way_nodes[current]] |= current_neighbours
+
+roads = dict()
+for node,node_neighbours in neighbours.items():
+    for neighbour in node_neighbours:
+        roads[tuple(sorted((node,neighbour)))] = 1 #Temporary
+
+def djikstra(nodes,roads,start,end,neighbour_dict):
+    path_dict = dict((node,(inf,None)) for node in nodes)
+    unvisited = set(nodes)
+    visited   = set()
+    path_dict[start] = (0,[start])
+    current = start
+    # print(neighbour_dict)
+    while True:
+        current_cost,current_path = path_dict[current]
+        neighbours = neighbour_dict[current]
+        for neighbour in neighbours:
+            if neighbour in unvisited:
+                neighbour_cost,neighbour_path = path_dict[neighbour]
+                road_name = tuple(sorted((current,neighbour)))
+                cost = roads[road_name] + current_cost
+                if cost < neighbour_cost:
+                    path_dict[neighbour] = (cost,current_path + [neighbour])
+        unvisited.remove(current)
+        visited.add(current)
+        min_cost,min_node = inf,None
+        for node in unvisited:
+            cost,path = path_dict[(node)]
+            if cost < min_cost:
+                min_cost,min_node = cost,node
+        if end in visited:
+            break
+        elif min_cost == inf:
+            return False
+        else:
+            current = min_node
+    return path_dict[end]
 
 start = 1741123983
 end = 1741123995
-start_node = new_nodes[start]
-end_node = new_nodes[end]
-print(start_node,end_node)
-print(node_dist(start_node,end_node))
+
+print(djikstra([node['id'] for node in nodes],roads,start,end,neighbours))
