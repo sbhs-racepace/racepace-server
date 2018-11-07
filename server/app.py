929cb3593c0835fc2fd108067379bd56a1b0dae7
sunnyyan01 <32755606+sunnyyan01@users.noreply.github.com>
2018-11-07 12:09:33 +1100

    Added caching of routes

--- server/app.py
+++ server/app.py
@@ -17,6 +17,7 @@ dotenv.load_dotenv()
 dev_mode = bool(int(os.getenv('development'))) # decides wether to deploy on server or run locally
 
 app = Sanic('majorproject')
+routes = {}
 
 async def fetch(url):
     """Makes a http get request"""
@@ -65,22 +66,31 @@ async def route(request):
 
     data = request.json # get paramaters from requester (location, preferences etc.)
     preferences = data.get('preferences') 
-    location = data.get('location')
+    start = data.get('start') #Lat + Lon seperated by comma
+    end = data.get('end')
+    
+    if start+end in routes:
+        response = {
+            'success': True,
+            'message': '',
+            'data': routes[start+end].json
+        }
+    else:
+        node_endpoint = Overpass.NODE.format(start+","+end)
+        way_endpoint = Overpass.WAY.format(start+","+end)
 
-    node_endpoint = Overpass.NODE.format(location)
-    way_endpoint = Overpass.WAY.format(location)
+        tasks = [fetch(node_endpoint), fetch(way_endpoint)]
+        nodedata, waydata = await asyncio.gather(*tasks) # concurrently make the two api calls
 
-    tasks = [fetch(node_endpoint), fetch(way_endpoint)]
-    nodedata, waydata = await asyncio.gather(*tasks) # concurrently make the two api calls
+        route = Route(nodedata['elements'], waydata['elements'], preferences, start, end)
+        route.generate_route()
+        routes[start+end] = route.json
 
-    route = Route(nodedata['elements'], waydata['elements'], preferences)
-    route.generate_route() 
-
-    response = {
-        'success': True,
-        'message': 'soz m8 not implemented yet',
-        'data': route.json
-    }
+        response = {
+            'success': True,
+            'message': '',
+            'data': route.json
+        }
 
     return json(response)
 
