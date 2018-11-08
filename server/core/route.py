fa37d43d39f1b9ffbdc7fa78ff6349c4519dfb39
Jason Yu <jasonyu0100@gmail.com>
2018-11-08 14:47:44 +1100

    Fixed unit conversion and add support of A* search and started coding preferences

--- server/core/route.py
+++ server/core/route.py
@@ -2,7 +2,7 @@ import math, copy
 import json
 from math import inf
 
-EARTH_RADIUS = 6371.009
+EARTH_RADIUS = 6371000 #In metres
 
 class Point:
     """
@@ -68,15 +68,16 @@ class Point:
 
 
 class Node:
-    def __init__(self, point, id):
+    def __init__(self, point: Point, id: str):
         self.id = id
         self.point = point
+        self.ways = set() #Set of ways that it is a part of
 
-    def __eq__(self, other):
+    def __eq__(self, other: Point):
         return self.id == other.id
 
     @staticmethod
-    def json_to_nodes(json_nodes):
+    def json_to_nodes(json_nodes: dict) -> dict:
         formatted_nodes = {}
         nodes = json_nodes['elements']
         for node in nodes:
@@ -84,20 +85,20 @@ class Node:
             formatted_nodes[node['id']] = Node(node_point,node['id'])
         return formatted_nodes
 
-    def closest_node(self, nodes):
-        """Returns the closest node from a list of nodes"""
+    def closest_node(self, nodes: dict) -> Point:
+        """Returns the closest node a dictionary of nodes"""
         nodes = sorted(nodes.values(), key=lambda n: n.pos - self.point)
         closest_node = nodes[0]
         return closest_node
 
 class Way:
-    def __init__(self, nodes, id, tags):
+    def __init__(self, nodes: list, id: str, tags):
         self.nodes = nodes
         self.id = id
         self.tags = tags
 
     @staticmethod
-    def json_to_ways(json_ways):
+    def json_to_ways(json_ways: dict) -> dict:
         formatted_ways = {}
         ways = json_ways['elements']
         for way in ways:
@@ -110,7 +111,10 @@ class Route:
         self.distance = distance
 
     @staticmethod
-    def find_neighbours(ways) -> dict:
+    def find_neighbours(ways: dict) -> dict:
+        """
+        Generates neighbours for every node in provided ways
+        """
         neighbours = {}
         for way in ways.values():
             way_nodes = way.nodes
@@ -124,36 +128,51 @@ class Route:
         return neighbours
 
     @staticmethod
-    def generate_route(nodes: dict, ways: dict, start: str, end: str, preferences: dict=None) -> None:
+    def generate_shortest_route(nodes: dict, ways: dict, start: str, end: str, preferences: dict=None) -> None:
+        """
+        Generates the shortest route to a destination
+        Uses A* with euclidean distance as heuristic
+        Uses tags to change cost of moving to nodes
+        """
         unvisited = set(node_id for node_id in nodes)
         visited = set()
         path_dict = dict((node,(inf,[node])) for node in nodes)
         neighbours = Route.find_neighbours(ways)
         path_dict[start] = (0,[start])
 
-        current = start
+        if end not in nodes: raise Exception('End node not in node space. Specify a valid node.')
+        elif start not in nodes: raise Exception('End node not in node space. Specify a valid node.')
+        else: current = start
+
         while True:
-            current_distance,current_path = path_dict[current]
+            current_cost,current_path = path_dict[current]
             current_neighbours = neighbours[current]
             for neighbour in current_neighbours:
                 if neighbour in unvisited:
-                    neighbour_distance,neighbour_path = path_dict[neighbour]
-                    neighbour_point = nodes[neighbour].point
-                    relative_distance = nodes[current].point.distance(neighbour_point)
-                    new_distance = relative_distance + current_distance
-                    if new_distance < neighbour_distance:
-                        path_dict[neighbour] = (new_distance,current_path + [neighbour])
+                    neighbour_cost,neighbour_path = path_dict[neighbour]
+                    relative_distance = nodes[current].point - nodes[neighbour].point
+                    # Added tag cost based on preferences of tags and distance as a scaling factor
+                    #neighbour_tags = nodes[neighbour].tags
+                    tag_cost = 0 #relative_distance * sum(preferences[tag] for tag in neighbour_tags)
+
+                    new_cost = relative_distance + current_cost + tag_cost
+                    if new_cost < neighbour_cost:
+                        path_dict[neighbour] = (new_cost,current_path + [neighbour])
             unvisited.remove(current)
             visited.add(current)
             if end in visited:
                 break
             else:
-                min_distance,next_node = inf, None
+                min_value,next_node = inf, None
                 for node_id in unvisited:
                     current_distance,path = path_dict[node_id]
-                    if current_distance < min_distance: min_distance,next_node = current_distance,node_id
-                if min_distance == inf:
-                    raise Exception("Can't get to end node")
+                    #Heuristic value uses distance to endpoint to judge closenss
+                    heuristic_value = nodes[node_id].point - nodes[end].point
+
+                    current_value = current_distance + heuristic_value
+                    if current_value < min_value: min_value,next_node = current_value,node_id
+                if min_value == inf:
+                    raise Exception("End node cannot be reached")
                 else:
                     current = next_node
 
@@ -161,9 +180,9 @@ class Route:
         return Route(fastest_route,route_distance)
 
 if __name__ == '__main__':
-    ways = Way.json_to_ways(json.loads(open('ways.json').read()))
+    ways  = Way.json_to_ways(json.loads(open('ways.json').read()))
     nodes = Node.json_to_nodes(json.loads(open('nodes.json').read()))
     start = 1741123995
-    end   = 1741124011
-    route = Route.generate_route(nodes,ways,start,end)
+    end   = 1741123987
+    route = Route.generate_shortest_route(nodes,ways,start,end)
     print(route.route,route.distance)
