efe6c6d6abb16e96ccf245b042e28913b5bf97ab
verixx <abdurraqeeb53@gmail.com>
2018-11-12 12:20:11 +1100

    Use discord webhooks to track status

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 import asyncio
+import subprocess
 import traceback
 import os
 
@@ -6,17 +7,24 @@ import dotenv
 import aiohttp
 
 from sanic import Sanic
-from sanic.exceptions import SanicException
+from sanic.exceptions import SanicException, abort
 from sanic.response import json
 
 from core.route import Route
 from core.endpoints import Overpass 
-from core.utils import timed, cached
+from core.utils import timed, memoized, run_with_ngrok
+from core.discord import Embed, Webhook
 
 dotenv.load_dotenv()
 dev_mode = bool(int(os.getenv('development'))) # decides wether to deploy on server or run locally
 
+status_icon = 'http://icons-for-free.com/free-icons/png/250/353838.png'
+
 app = Sanic('majorproject')
+
+if dev_mode:
+    run_with_ngrok(app)
+
 routes = {}
 
 async def fetch(url):
@@ -27,9 +35,21 @@ async def fetch(url):
 @app.listener('before_server_start')
 async def init(app, loop):
     app.session = aiohttp.ClientSession(loop=loop) # we use this to make web requests
+    app.webhook = Webhook(os.getenv('webhook_url'), session=app.session, is_async=True)
+
+    em = Embed(color=0x00FF00, timestamp=True)
+    em.set_author('Status: Server Starting', icon_url=status_icon)
+    em.add_field('Public url', app.ngrok_url, inline=False)
+    em.add_field('Domain', 'http://localhost:8000')
+
+    await app.webhook.send(embeds=em)
 
 @app.listener('after_server_stop')
 async def aexit(app, loop):
+    em = Embed(color=0xff3300, timestamp=True)
+    em.set_author('Status: Server Stopped', icon_url=status_icon)
+
+    await app.webhook.send(embeds=em)
     await app.session.close()
 
 @app.exception(Exception)
@@ -58,14 +78,14 @@ async def index(request):
 
     return json(data)
 
-
 @app.get('/api/route')
 @timed
 @memoized
 async def route(request):
     '''Api endpoint to generate the route'''
 
-    data = request.json # get paramaters from requester (location, preferences etc.)
+    data = request.json if request.json else abort(400, 'Request must have a json body.')
+
     preferences = data.get('preferences')
     bounding_box = data.get('bounding_box')
     start = data.get('start') #Lat + Lon seperated by comma
