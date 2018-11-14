596bd25f1bcb99eae0dc9d8f473150f34eb56a1a
verixx <abdurraqeeb53@gmail.com>
2018-11-14 21:35:47 +1100

    rename

--- server/core/enum.py
+++
@@ -1,9 +0,0 @@
-class Overpass:
-    BASE = 'http://overpass-api.de/api/interpreter?data=[out:json];'
-    NODE = BASE + 'node({});out;'
-    WAY = BASE + 'way({});out;'
-
-class Color:
-    green = 0x2ecc71
-    red = 0xe74c3c
-    orange = 0xe67e22
