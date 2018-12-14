7fd6564485d3a1c0be31ff38e64a1a4360d86d3f
Jason Yu <jasonyu0100@gmail.com>
2018-12-14 19:58:31 +1100

    Added Map

--- racepace/App.js
+++ racepace/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Platform, View, Text, Button } from "react-native";
+import { Platform, View, Text, Button, StyleSheet } from "react-native";
+import MapView from 'react-native-maps';
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
 const instructions = Platform.select({
@@ -19,6 +20,66 @@ class HomeScreen extends React.Component {
           title="Go to Details Screen"
           onPress={() => this.props.navigation.navigate('Details')}
         />
+        <Button
+          title="Go to Map Screen"
+          onPress={() => this.props.navigation.navigate('Map')}
+        />
+      </View>
+    );
+  }
+}
+
+class MapScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+      position: {
+        latitude: 37.78825,
+        longitude: -122.4324
+      }
+    };
+  }
+
+  onRegionChange = (region) => {
+    position = {
+      latitude: region.latitude,
+      longitude: region.longitude
+    }
+    this.setState({ region , position});
+  }
+
+  render() {
+    const styles = StyleSheet.create({
+      container: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+      },
+      map: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+      },
+    });
+
+    return (
+      <View style={styles.container}>
+      <MapView style={styles.map}
+        initialRegion={this.state.region}
+        onRegionChange={this.onRegionChange}
+      />
       </View>
     );
   }
@@ -29,10 +90,6 @@ class DetailsScreen extends React.Component {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Details Screen</Text>
-        <Button
-          title="Go to Home Screen"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
       </View>
     );
   }
@@ -41,7 +98,8 @@ class DetailsScreen extends React.Component {
 const AppNavigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Details: DetailsScreen
+    Details: DetailsScreen,
+    Map: MapScreen,
   },
   {
     initialRouteName: "Home"
