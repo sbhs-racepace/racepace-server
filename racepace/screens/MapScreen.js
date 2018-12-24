fbb342133d18b4c55f08e6b0462a8793b96bf6a2
Jason Yu <jasonyu0100@gmail.com>
2018-12-24 15:16:59 +1100

    Working Route Map Marker

--- racepace/screens/MapScreen.js
+++ racepace/screens/MapScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
 import { View, Text, StyleSheet } from "react-native";
 
 export default class MapScreen extends React.Component {
@@ -7,16 +8,38 @@ export default class MapScreen extends React.Component {
     super(props);
     this.state = {
       region: {
-        latitude: 37.78825,
-        longitude: -122.4324,
+        latitude: -33.965832,
+        longitude: 151.089029,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
       },
       position: {
-        latitude: 37.78825,
-        longitude: -122.4324
-      }
+        latitude: -33.965832,
+        longitude: 151.089029,
+      },
+      markers: []
     };
+  }
+
+  componentDidMount() {
+    data = {
+        'size': 1000,
+        'start': '-33.965832, 151.089029',
+        'end': '-33.964693, 151.090788'
+    }
+    fetch('http://127.0.0.1:8000/api/route',{
+      method: "POST",
+      body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(result => {
+      console.log(result.route)
+      result.route.map(marker => {
+        this.state.markers.push({
+          title:'Hello',
+          coordinates: marker
+        })})
+    })
   }
 
   onRegionChange = (region) => {
@@ -24,7 +47,7 @@ export default class MapScreen extends React.Component {
       latitude: region.latitude,
       longitude: region.longitude
     }
-    this.setState({ region , position});
+    this.setState({region , position});
   }
 
   render() {
@@ -48,11 +71,24 @@ export default class MapScreen extends React.Component {
     });
 
     return (
+      // <View style={styles.container}>
+      // <MapView style={styles.map}
+      //   initialRegion={this.state.region}
+      //   onRegionChange={this.onRegionChange}
+      // <Marker coordinate={marker.coordinates[0].coordinate}></Marker>
+      // </MapView>
+      // </View>
+
+
       <View style={styles.container}>
-      <MapView style={styles.map}
-        initialRegion={this.state.region}
-        onRegionChange={this.onRegionChange}
-      />
+        <MapView style={styles.map}
+          initialRegion={this.state.region}
+          onRegionChange={this.onRegionChange}>
+          {this.state.markers.map(marker => (
+            <Marker coordinate={marker.coordinates}>
+            </Marker>
+          ))}
+        </MapView>
       </View>
     );
   }
