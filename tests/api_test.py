61c637eefb14b87c2194fa0bbc4eaf0f884a4ef1
verixx <abdurraqeeb53@gmail.com>
2018-12-27 15:49:56 +1100

    Add test api client

--- tests/api_test.py
+++ tests/api_test.py
@@ -1,38 +1,74 @@
 import requests
 import json
-from yarl import URL
 
-api = URL('http://127.0.0.1:8000/api')
+class TestApiClient:
+    BASE = 'http://127.0.0.1:8000/api'
 
+    def __init__(self, token=None):
+        self.session = requests.Session()
+        self.token = token
+        self.user_id = 0
+        if token:
+            self.session.headers = {
+                'Authorization': 'Bearer ' + token
+            }
+    
+    def register_user(self, **credentials):
+        url = self.BASE + '/register'
+        resp = self.session.post(url, json=credentials).json()
+        if resp['success']:
+            print('Created user with email: {email} and password: {password}'.format(**credentials))
+        else:
+            print(resp['error'])
+        return resp
+    
+    def login(self, **credentials):
+        url = self.BASE + '/login'
+        resp = self.session.post(url, json=credentials)
+        data = resp.json()
+        if data['success']:
+            self.session.headers = {
+                'Authorization': 'Bearer '+ data['token']
+                }
+            self.user_id = data['user_id']
+            print(f"Logged into user account with id: {data['user_id']}")
+        else:
+            print(data['error'])
+        return data
+    
+    def get_route(self, data=None):
+        url = self.BASE + '/route'
 
-def test_route():
-    data = {
+        data = data or {
         'size': 1000, # meters
         'start': '-33.965832, 151.089029',
         'end': '-33.964693, 151.090788'
-    }
+        }
+        resp = self.session.post(url, json=data)
+        return resp.json()
 
-    resp = requests.post(api / 'route', json=data)
+    def delete_account(self, user_id=None):
+        user_id = user_id or self.user_id
+        url = self.BASE + f'/users/{user_id}'
+        resp = self.session.delete(url).json()
+        if resp['success']:
+            print(f'Deleted user account with id: {user_id}')
+        else:
+            print(resp['error'])
+        return resp
 
-    print(resp)
-    print(resp.json())
+client = TestApiClient()
 
+client.register_user(email='gahugga@gmail.com', password='bobby')
 
-def login(**credentials):
-    url = api/'login'
-    print(url)
-    resp = requests.post(url, json=credentials)
-    print(resp)
-    print(resp.json())
+user = client.login(email='gahugga@gmail.com', password='bobby')
 
+# route = client.get_route()
 
-def register(**credentials):
-    url = api/'register'
-    resp = requests.post(url, json=credentials)
-    print(resp)
-    print(resp.json())
+# if route.get('route'):
+#     print(json.dumps(route['route'], indent=4))
+# else:
+#     print(route['error'])
 
-
-login(email='testing@gmail.com', password='testing123')
-
+client.delete_account()
 
