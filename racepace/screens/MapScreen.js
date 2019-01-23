ecfbabbe230ef12e729ff18964642409a955fa06
Jason Yu <jasonyu0100@gmail.com>
2019-01-23 12:19:42 +1100

    attempt at fixing maps

--- racepace/screens/MapScreen.js
+++ racepace/screens/MapScreen.js
@@ -1,22 +1,32 @@
 import React from "react";
 import MapView from 'react-native-maps';
 import { Marker, Polyline } from 'react-native-maps';
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, Dimensions } from "react-native";
+
+let { width, height } = Dimensions.get('window');
+
+const LATITUDE_DELTA = 0.0922*1.5
+const LONGITUDE_DELTA = 0.0421*1.5
+
+export const getCurrentLocation = () => {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(position => resolve(position), e => reject(e));
+  });
+};
 
 export default class MapScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       region: {
-        latitude: -33.965832,
-        longitude: 151.089029,
-        latitudeDelta: 0.0922*1.5,
-        longitudeDelta: 0.0421*1.5,
+        latitude: -33.9672563,
+        longitude: 151.1002119,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
       },
-      lastLat: null,
-      lastLong: null,
       markers: []
     };
+    
     let start = '-33.9672563,151.1002119'
     let end   = '-33.9619925,151.1059193'
     this.api_route(start, end);
@@ -41,23 +51,73 @@ export default class MapScreen extends React.Component {
     })
   }
 
+  // setToLocation(location) {
+  //   let region = {
+  //     latitude: location.latitude,
+  //     longitude: location.longitude,
+  //     latitudeDelta: 0.0922*1.5,
+  //     longitudeDelta: 0.0421*1.5,
+  //   }
+  //   this.onRegionChange(region);
+  // }
+
+  // setToCurrentLocation() {
+  //   navigator.geolocation.getCurrentPosition((position) => {
+  //     let location = {
+  //       latitude: position.latitude,
+  //       longitude:position.longitude
+  //     }
+  //     this.setToLocation(location);
+  //   }, (error) => {
+  //     alert(JSON.stringify(error))
+  //   }, {
+  //     enableHighAccuracy: true,
+  //     timeout: 20000,
+  //     maximumAge: 1000
+  //   });
+  // }
+
+  // componentDidMount() {
+  //   this.setToCurrentLocation()
+  // }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          },
+          markers:this.state.markers
+        });
+      },
+    (error) => console.log(error.message),
+    { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+    this.watchID = navigator.geolocation.watchPosition(
+      position => {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          },
+          markers:this.state.markers
+        });
+      }
+    );
+    console.log(this.state.region)
+  }
+
   onRegionChange = (region) => {
     this.setState({
       mapRegion: region,
       lastLat: region.latitude || this.state.lastLat,
       lastLong: region.longitude || this.state.lastLong
-    });
-  }
-
-  componentDidMount() {
-    this.watchID = navigator.geolocation.watchPosition((position) => {
-      let region = {
-        latitude:       position.coords.latitude,
-        longitude:      position.coords.longitude,
-        latitudeDelta:  0.00922*1.5,
-        longitudeDelta: 0.00421*1.5
-      }
-      this.onRegionChange(region);
     });
   }
 
@@ -70,9 +130,8 @@ export default class MapScreen extends React.Component {
       <MapView
         style={{flex: 1}}
         showsUserLocation={true}
-        followUserLocation={true}
         initialRegion={this.state.region}
-        onRegionChange={this.onRegionChange}
+        onRegionChange={this.onRegionChange.bind(this)}
       >
         <Polyline
           coordinates={this.state.markers.map(marker => {return marker.coordinate})}
