74c52fae06e45c48ed5f4e5f32c098a0fe1bfc44
Jason Yu <jasonyu0100@gmail.com>
2019-02-27 17:30:26 +1100

    renamed files

--- tests/api_testing/api_route_test.py
+++ tests/api_testing/api_route_test.py
@@ -3,7 +3,7 @@ from client import TestApiClient
 
 client = TestApiClient()
 
-start = ["-33.8776173,151.2012087", 'hi']
+start = "-33.8776173,151.2012087"
 end = "-33.8831174,151.2045339"
 
 route = client.get_route(start,end)
