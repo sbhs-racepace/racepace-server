200495e159418616e89beeba97f9e33590bb404a
Jason Yu <jasonyu0100@gmail.com>
2019-03-01 13:49:10 +1100

    multiple route testing

--- tests/api_testing/api_multiple_route_test.py
+++ tests/api_testing/api_multiple_route_test.py
@@ -2,9 +2,9 @@ from client import TestApiClient
 
 
 client = TestApiClient()
+waypoints = ["-33.892545,151.219588","-33.892023,151.216920"]
+#,"-33.886291, 151.217958"]
 
-waypoints = ["-33.892237, 151.220249", '-33.892101, 151.211785', '-33.883380, 151.207635', '-33.876070, 151.218543']
-
-route = client.get_multiple_route(waypoints)
+route = client.get_route(start="-33.892545,151.219588",end="-33.892023,151.216920")
 
 print(route)        
