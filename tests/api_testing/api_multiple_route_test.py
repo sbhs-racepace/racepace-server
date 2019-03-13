311891aed1d1f0738bfb6d80f91bef66f077b3d9
Jason Yu <jasonyu0100@gmail.com>
2019-03-14 09:49:11 +1100

    Added proc file, and runtime for heroku support

--- tests/api_testing/api_multiple_route_test.py
+++ tests/api_testing/api_multiple_route_test.py
@@ -2,9 +2,6 @@ from client import TestApiClient
 
 
 client = TestApiClient()
-waypoints = ["-33.892545,151.219588","-33.892023,151.216920"]
-#,"-33.886291, 151.217958"]
-
-route = client.get_route(start="-33.892545,151.219588",end="-33.892023,151.216920")
-
+waypoints = ["-33.892098, 151.216802","-33.886053, 151.218030","-33.880612, 151.216838"]
+route = client.get_multiple_route(waypoints)
 print(route)        
