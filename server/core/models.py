65fdeaef961e4aa247712cd7e02b428397bc97f6
Jason Yu <jasonyu0100@gmail.com>
2019-04-27 13:56:12 +1000

    fixing local/database retrieval

--- server/core/models.py
+++ server/core/models.py
@@ -44,8 +44,9 @@ class User:
         Generates User class from python data
         Abdur Raqeeb
         """
-        print(data)
-        routes,credentials = data.pop('routes'),data.pop('credentials')
+        if 'user_id' in data: data['_id'] = data.pop('user_id') # Supporting Database/Local retrieval 
+        routes, credentials = data.pop('routes'),data.pop('credentials')
+        data['credentials'] = Credentials(**credentials)
         user = cls(app=app, **data)
         user.routes = routes
         return user
