a841b7e647bc0e7a020ef8ffa382395d7c58dd44
Kyber <abdurraqeeb53@gmail.com>
2019-06-11 12:30:05 +1000

    [stats] Add protected user stats page

--- server/core/stats.py
+++ server/core/stats.py
@@ -1,5 +1,25 @@
+from functools import wraps
+
 from sanic import Blueprint, response
 
+
+
+def authrequired(func):
+    """
+    Abdur Raqeeb
+    """    
+    @wraps(func)
+    async def wrapper(request, *args, **kwargs):
+        if not request['session'].get('logged_in'):
+            return response.redirect('/login')
+        token = request['session'].get('token')
+        user_id = jwt.decode(request.token, request.app.secret)['sub']
+        user = await request.app.users.find_account(_id=user_id)
+        if user:
+            return await func(request, user, *args, **kwargs)
+        else:
+            abort(401, "Invalid token")
+    return wrapper
 
 stats = Blueprint('stats')
 
@@ -26,6 +46,14 @@ async def login(request):
     
     request['session']['logged_in'] = True
     request['session']['token'] = token
+    
+    return response.redirect('/stats')
+
+@stats.get('/stats')
+@authrequired()
+async def show_stats(request, user):
+    return response.text(user.stats.to_dict()))
+
     
     
   
