e3c68611af74ac1e37f5a8d711e32e966a9b66e8
Jason Yu <jasonyu0100@gmail.com>
2019-07-10 22:52:32 +1000

    Update user.py

--- server/core/user.py
+++ server/core/user.py
@@ -51,10 +51,12 @@ class User:
         """
 
         user_id = data.pop("_id")
+        print(data["saved_routes"].values())
         data["saved_routes"] = {
             saved_route_data['id'] : SavedRoute.from_data(saved_route_data)
             for saved_route_data in data["saved_routes"].values()
         }
+        print(data["saved_runs"].values())
         data["saved_runs"] = {
             saved_run_data['id'] : SavedRun.from_data(saved_run_data)
             for saved_run_data in data["saved_runs"].values()
