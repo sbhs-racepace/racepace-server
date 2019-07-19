97d5f6eda6868cf21d87c7819060f86df721baa0
Jason Yu <jasonyu0100@gmail.com>
2019-07-19 14:50:41 +1000

    Update api_multiple_route_test.py

--- tests/api_testing/api_multiple_route_test.py
+++ tests/api_testing/api_multiple_route_test.py
@@ -3,9 +3,9 @@ from client import TestApiClient
 
 client = TestApiClient()
 waypoints = [
-    "-33.892098, 151.216802",
-    "-33.886053, 151.218030",
-    "-33.880612, 151.216838",
+    "-33.892098,151.216802",
+    "-33.886053,151.218030",
+    "-33.880612,151.216838",
 ]
 route = client.get_multiple_route(waypoints)
 print(route)
